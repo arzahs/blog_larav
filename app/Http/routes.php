45ebf70c7ac4b293f825d42cc1a87bf7b99ced55
Sergey Nelepa <nelepa1995@mail.ru>
2015-08-17 20:10:10 +0300

    Добавил контроллер постов и вьюшку для него же, все это прописал в роутах

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+get('/', ['as' => 'post', 'uses' => 'PostController@index']);
+
+
